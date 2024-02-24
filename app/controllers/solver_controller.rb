7ef084115b7b88afe3d6eb76e8a5de791028ef98
Oleg Popkov <amik0amik0@gmail.com>
2024-02-24 18:00:42 +0300

    docker

--- app/controllers/solver_controller.rb
+++ app/controllers/solver_controller.rb
@@ -1,11 +1,13 @@
 class SolverController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     attempts = params[:attempts].map do |attempt|
       attempt.map { |letter| [letter.first, letter.second.to_sym] }
     end
 
     solver = Solver.new
-    result = nil
+    result = []
 
     attempts.each do |attempt|
       result = solver.add_attempt(attempt)
